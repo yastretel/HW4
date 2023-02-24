774dc186ab19ebd8b676b047f42060ce141277a0
Yaroslav <yas.tretel@gmail.com>
2023-02-24 19:26:15 +0300

    HW Done

---
+++ Task 25/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int Exponentiation(int numberA, int numberB){
+    int result = 1;
+    for(int i=1; i <= numberB; i++){
+        result = result * numberA;
+    }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Ответ: " + exponentiation);
